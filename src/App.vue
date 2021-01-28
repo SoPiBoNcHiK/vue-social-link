ed916cfcec9cae1103cb23733d963d5b8d54783d
sopibonchik <sopibonchik@gmail.com>
2021-01-28 19:47:57 +0300

    new nac

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="nav"
+      app
+      absolute
+      temporary
+    ></v-navigation-drawer>
+    <v-content app>
+      <v-btn v-on:click="nav = !nav">Меню</v-btn>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+
+  },
+
+  data: () => ({
+    nav: false
+  }),
+};
+</script>
