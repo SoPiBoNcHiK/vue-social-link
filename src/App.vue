0e5f23e37a0c4fb4c3d3fbb9f34728d738e6c8b0
sopibonchik <sopibonchik@gmail.com>
2021-01-28 20:12:18 +0300

    Added avatar and name

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,33 @@
     <v-navigation-drawer
       v-model="nav"
       app
-      absolute
-      temporary
-    ></v-navigation-drawer>
+      color="green"
+      dark
+      expand-on-hover
+      hide-overlay
+      permanent
+      right
+    >
+    <v-list
+      nav
+      shaped
+      dense
+    >
+      <v-list-item two-line>
+        <v-list-item-avatar>
+          <img src="https://randomuser.me/api/portraits/men/5.jpg">
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>
+            SocialLink
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Кто-то там
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    </v-navigation-drawer>
     <v-content app class="px-12 py-3">
       <v-container fluid>
       <router-view></router-view>
